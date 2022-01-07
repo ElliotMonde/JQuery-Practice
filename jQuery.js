1eaa470761adce78211d5121282025bec29ec4e6
ElliotMonde <elliotphua@gmail.com>
2022-01-07 18:37:54 +0800

    jquery practice completed 19 Nov 2021

---
+++ jQuery.js
@@ -0,0 +1,4 @@
+//jQuery JavaScript Document
+
+$("h1").css("font-family","arial").css("color","navy").css("font-size","4rem").css("margin","0")  ;
+$("h2").css("margin","0 0 2% 0").css("font-family","arial");
